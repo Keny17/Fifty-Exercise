6a2c4ef575febb9088429c3001d6b30c9e8e76fc
kenymunoz96@gmail.com <kenymunoz96@gmail.com>
2022-05-07 13:25:22 -0500

    Start my Project

---
+++ main.js
@@ -0,0 +1,34 @@
+//It allows you to get the largest of three numbers.
+
+function largestNumber(n1=5,n2=7,n3=2){
+    let num1 = n1, num2 = n2, num3 = n3;
+    let mayor;
+
+    if((num1 >= num2)&&(num1>=num3)){
+
+        mayor.innerHTML = ' <h1> ${num1} </h1>';
+    
+    }else if(num2 > num3){
+
+        mayor.innerHTML = ' <h1> ${num2} </h1>';
+
+    }else{
+
+        mayor.innerHTML = ' <h1> ${num2} </h1>';
+
+    }
+     
+}
+
+
+function sentenceLower(){
+
+const  worldChange = ('I love you');
+
+function worldChange(sentence){
+
+sentence.toUpperCase()
+
+}
+
+}
