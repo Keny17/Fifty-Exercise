080f41a9439bcf947927eb736ecbbc9512f6bd00
kenymunoz96@gmail.com <kenymunoz96@gmail.com>
2022-06-29 11:30:13 -0500

    Changes in content by html and css

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //It allows you to get the largest of three numbers.
 
-function largestNumber(n1=5,n2=7,n3=2){
+function largestNumber(n1,n2,n3){
     let num1 = n1, num2 = n2, num3 = n3;
-    let mayor;
+    let mayor=0;
 
     if((num1 >= num2)&&(num1>=num3)){
 
